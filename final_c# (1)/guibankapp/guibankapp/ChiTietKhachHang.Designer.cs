9c98f9f01658a1005561cf9623228a33333acc1a
LeTranAnhKietHCMUS <22207116@student.hcmus.edu.vn>
2025-07-24 11:41:06 +0700

    project THOOP (word bao cao va code c#)

---
+++ final_c# (1)/guibankapp/guibankapp/ChiTietKhachHang.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace guibankapp
+{
+    partial class ChiTietKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMaKH = new System.Windows.Forms.Label();
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.lblCMND = new System.Windows.Forms.Label();
+            this.lblDiaChi = new System.Windows.Forms.Label();
+            this.lblSoDT = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblSoTaiKhoan = new System.Windows.Forms.Label();
+            this.listViewTaiKhoan = new System.Windows.Forms.ListView();
+            this.btnDong = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMaKH
+            // 
+            this.lblMaKH.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblMaKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaKH.Location = new System.Drawing.Point(331, 100);
+            this.lblMaKH.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMaKH.Name = "lblMaKH";
+            this.lblMaKH.Size = new System.Drawing.Size(347, 38);
+            this.lblMaKH.TabIndex = 0;
+            this.lblMaKH.Text = "label1";
+            this.lblMaKH.Click += new System.EventHandler(this.lblMaKH_Click);
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblHoTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoTen.Location = new System.Drawing.Point(331, 179);
+            this.lblHoTen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHoTen.Name = "lblHoTen";
+            this.lblHoTen.Size = new System.Drawing.Size(347, 38);
+            this.lblHoTen.TabIndex = 1;
+            this.lblHoTen.Text = "label2";
+            this.lblHoTen.Click += new System.EventHandler(this.lblHoTen_Click);
+            // 
+            // lblCMND
+            // 
+            this.lblCMND.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblCMND.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCMND.Location = new System.Drawing.Point(331, 258);
+            this.lblCMND.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCMND.Name = "lblCMND";
+            this.lblCMND.Size = new System.Drawing.Size(347, 38);
+            this.lblCMND.TabIndex = 2;
+            this.lblCMND.Text = "label3";
+            // 
+            // lblDiaChi
+            // 
+            this.lblDiaChi.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblDiaChi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiaChi.Location = new System.Drawing.Point(331, 329);
+            this.lblDiaChi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDiaChi.Name = "lblDiaChi";
+            this.lblDiaChi.Size = new System.Drawing.Size(347, 38);
+            this.lblDiaChi.TabIndex = 3;
+            this.lblDiaChi.Text = "label4";
+            // 
+            // lblSoDT
+            // 
+            this.lblSoDT.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblSoDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoDT.Location = new System.Drawing.Point(331, 410);
+            this.lblSoDT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSoDT.Name = "lblSoDT";
+            this.lblSoDT.Size = new System.Drawing.Size(347, 38);
+            this.lblSoDT.TabIndex = 4;
+            this.lblSoDT.Text = "label5";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(331, 485);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(347, 38);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "label6";
+            // 
+            // lblSoTaiKhoan
+            // 
+            this.lblSoTaiKhoan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblSoTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoTaiKhoan.Location = new System.Drawing.Point(331, 558);
+            this.lblSoTaiKhoan.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSoTaiKhoan.Name = "lblSoTaiKhoan";
+            this.lblSoTaiKhoan.Size = new System.Drawing.Size(347, 38);
+            this.lblSoTaiKhoan.TabIndex = 6;
+            this.lblSoTaiKhoan.Text = "label7";
+            // 
+            // listViewTaiKhoan
+            // 
+            this.listViewTaiKhoan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listViewTaiKhoan.BackColor = System.Drawing.Color.OldLace;
+            this.listViewTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listViewTaiKhoan.HideSelection = false;
+            this.listViewTaiKhoan.Location = new System.Drawing.Point(733, -56);
+            this.listViewTaiKhoan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listViewTaiKhoan.Name = "listViewTaiKhoan";
+            this.listViewTaiKhoan.Size = new System.Drawing.Size(1471, 868);
+            this.listViewTaiKhoan.TabIndex = 7;
+            this.listViewTaiKhoan.UseCompatibleStateImageBehavior = false;
+            this.listViewTaiKhoan.SelectedIndexChanged += new System.EventHandler(this.listViewTaiKhoan_SelectedIndexChanged);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnDong.BackColor = System.Drawing.Color.LightCoral;
+            this.btnDong.Location = new System.Drawing.Point(5, 635);
+            this.btnDong.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(100, 68);
+            this.btnDong.TabIndex = 8;
+            this.btnDong.Text = "DONG";
+            this.btnDong.UseVisualStyleBackColor = false;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 94);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 30);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Ma Khach Hang";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 179);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 30);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ho va ten";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 258);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 30);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "CMND";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(4, 329);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 30);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Dia chi";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(4, 410);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 30);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "So dien thoai";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(4, 485);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 30);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(4, 558);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(250, 30);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "So tai khoan da mo";
+            // 
+            // ChiTietKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2053, 806);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.listViewTaiKhoan);
+            this.Controls.Add(this.lblSoTaiKhoan);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblSoDT);
+            this.Controls.Add(this.lblDiaChi);
+            this.Controls.Add(this.lblCMND);
+            this.Controls.Add(this.lblHoTen);
+            this.Controls.Add(this.lblMaKH);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ChiTietKhachHang";
+            this.Text = "ChiTietKhachHang";
+            this.Load += new System.EventHandler(this.ChiTietKhachHang_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMaKH;
+        private System.Windows.Forms.Label lblHoTen;
+        private System.Windows.Forms.Label lblCMND;
+        private System.Windows.Forms.Label lblDiaChi;
+        private System.Windows.Forms.Label lblSoDT;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblSoTaiKhoan;
+        private System.Windows.Forms.ListView listViewTaiKhoan;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
